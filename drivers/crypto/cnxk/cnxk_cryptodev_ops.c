e0edb3f2e4255efe80f755a66095a2a43b516050
Sucharitha Sarananaga <ssarananaga@marvell.com>
2025-06-12 05:49:19 -0400

    crypto/cnxk: add new API to get fpm tbl address
    This patch introduces a new API to get FPM table host address.
    
    Signed-off-by: Sucharitha Sarananaga <ssarananaga@marvell.com>
    Change-Id: Ifa92d4184544d3aa902a0c7cc5f3aec442180ebc
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/dataplane/dpdk/+/155546
    Base-Builds: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>
    Base-Tests: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>
    Tested-by: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>
    Reviewed-by: Anoob Joseph <anoobj@marvell.com>

--- drivers/crypto/cnxk/cnxk_cryptodev_ops.c
+++ drivers/crypto/cnxk/cnxk_cryptodev_ops.c
@@ -11,6 +11,7 @@
 #endif
 #include <rte_security_driver.h>
 
+#include "roc_ae.h"
 #include "roc_ae_fpm_tables.h"
 #include "roc_cpt.h"
 #include "roc_errata.h"
@@ -991,6 +992,27 @@ rte_pmd_cnxk_crypto_qptr_get(uint8_t dev_id, uint16_t qp_id)
 	qptr = fp_ops->qp.data[qp_id];
 
 	return qptr;
+}
+
+const uint64_t *
+rte_pmd_cnxk_ae_fpm_table_get(uint8_t dev_id)
+{
+	struct rte_cryptodev *dev;
+	struct cnxk_cpt_vf *vf;
+
+	dev = rte_cryptodev_pmd_get_dev(dev_id);
+	if (dev == NULL) {
+		plt_err("Invalid dev_id %u", dev_id);
+		return NULL;
+	}
+
+	vf = dev->data->dev_private;
+	if (vf == NULL) {
+		plt_err("VF is not initialized");
+		return NULL;
+	}
+
+	return vf->cnxk_fpm_iova;
 }
 
 static inline void
